756d2d937269fe044fa21e87780e1d3c1433a1ec
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-27 20:39:57 -0300

    feat: removed basic project files

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -7,17 +7,4 @@ describe('App', () => {
       imports: [App],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, jogo-da-memoria');
-  });
 });
