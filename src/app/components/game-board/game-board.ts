f5dd95f89aca2b159d21c7a3be493f3b48b471db
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-28 18:38:02 -0300

    feat: matrix generate

--- src/app/components/game-board/game-board.ts
+++ src/app/components/game-board/game-board.ts
@@ -1,20 +1,23 @@
-import { Component, effect, input } from '@angular/core';
+import { Component, computed, effect, input } from '@angular/core';
 import { IGameDifficulty, IGameMode } from "../../constants/game";
+import { GameCard } from "../game-card/game-card";
 
 @Component({
   selector: 'app-game-board',
-  imports: [],
+  imports: [GameCard],
   templateUrl: './game-board.html',
   styleUrl: './game-board.css',
 })
 export class GameBoard {
   readonly gameConfig = input.required<{gamemode: IGameMode, difficulty: IGameDifficulty } | null>();
+  protected items_count = 0;
+  auxGameArray = computed(() => Array.from({ length: this.items_count}, (_, i) => i));
   
   constructor() {
     effect(() => {
       const config = this.gameConfig();
       if(config) {
-        console.log(config);
+        this.items_count = config.difficulty.items_count;
       }
     });
   }
