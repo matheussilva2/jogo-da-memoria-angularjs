c1de0d90367394934845970e8d63f5c8605bc0c8
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-28 12:08:06 -0300

    feat: implementation of authentication at home

--- src/app/screens/home/home.ts
+++ src/app/screens/home/home.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from "../../services/auth";
+import { FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  protected auth = inject(AuthService);
+  nameControl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(3)]
+  });
 
+  login(event: SubmitEvent) {
+    event.preventDefault();
+
+    const player_name = this.nameControl.value;
+
+    if(this.nameControl.valid) {
+      this.auth.login(player_name);
+    }
+  }
 }
