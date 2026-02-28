4b9db255f6dd1ba89dac14289215e009a8e94cfa
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-28 12:07:54 -0300

    feat: authentication service

---
+++ src/app/services/auth.ts
@@ -0,0 +1,45 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { Player } from "../models/player.model";
+
+const BASIC_PLAYER_MOCK: Player = {
+  name: "",
+  high_score: 0,
+  match_history: []
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private _player = signal<Player | null>(null);
+
+  readonly player = this._player.asReadonly();
+  readonly isAuthenticated = computed(() => !!this._player());
+
+  constructor() {
+    this.loadSession();
+  }
+
+  login(player_name: string) {
+    this._player.update((current: Player | null) => {
+      if(!current){
+        const new_player = BASIC_PLAYER_MOCK;
+        new_player.name = player_name;
+
+        return new_player;
+      }
+
+      const updatedPlayer: Player = {
+        ...current,
+        name: player_name
+      }
+
+      return updatedPlayer;
+    });
+  }
+
+  private loadSession() {
+    const data = localStorage.getItem('jm_player_data');
+    if (data) this._player.set(JSON.parse(data));
+  }
+}
