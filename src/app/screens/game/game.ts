672f5fda761136a2bb5962f9b4c73bdf0e8974b2
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-28 18:38:14 -0300

    feat: implemented time counting

--- src/app/screens/game/game.ts
+++ src/app/screens/game/game.ts
@@ -1,13 +1,14 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { toSignal } from "@angular/core/rxjs-interop";
 import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { map } from "rxjs";
 import { GAME_DIFFICULTIES, GAME_MODES, IGameDifficulty, IGameMode } from "../../constants/game";
 import { GameBoard } from "../../components/game-board/game-board";
+import { TimeFormatPipe } from "../../pipes/time-format-pipe";
 
 @Component({
   selector: 'app-game',
-  imports: [RouterLink, GameBoard],
+  imports: [RouterLink, GameBoard, TimeFormatPipe],
   templateUrl: './game.html',
   styleUrl: './game.css',
 })
@@ -30,9 +31,34 @@ export class Game {
   );
 
   readonly gameConfig = signal<{gamemode: IGameMode, difficulty: IGameDifficulty } | null>(null);
+  readonly remaining_time = signal<number>(0);
+  private timer_id: any;
+  private game_started:boolean = false;
 
   constructor() {
     this.loadGameConfig();
+
+    effect(() => {
+      if(this.game_started && this.remaining_time() < 0) {
+        this.stopGame();
+      }
+    });
+  }
+
+  startGame() {
+    if(this.timer_id) return;
+
+    this.timer_id = setInterval(() => {
+      this.remaining_time.update((prev) => prev-1);
+    }, 1000);
+  }
+
+  stopGame() {
+    if(this.timer_id) {
+      clearInterval(this.timer_id);
+      this.timer_id = undefined;
+      alert("Tempo esgotado!");
+    }
   }
 
   loadGameConfig() {
@@ -46,6 +72,10 @@ export class Game {
       gamemode: gamemode[0],
       difficulty: difficulty[0]
     });
+
+    this.remaining_time.set(difficulty[0].time);
+
+    this.startGame();
 
     return;
   }
