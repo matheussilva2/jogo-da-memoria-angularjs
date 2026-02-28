5a492cdef868c622ae5dee6ddd8b2b0c3cfffefe
Matheus <matheusnascimentoeoq@gmail.com>
2026-02-28 15:29:04 -0300

    feat: added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { Home } from "./screens/home/home";
+import { Gamemode } from "./screens/gamemode/gamemode";
+import { History } from "./screens/history/history";
+import { Game } from "./screens/game/game";
 
 export const routes: Routes = [
     {
         path: '',
         component: Home
+    },
+    {
+        path: 'jogar',
+        component: Gamemode
+    },
+    {
+        path: 'historico',
+        component: History
+    },
+    {
+        path: 'jogar/:slug',
+        component: Game
     }
 ];
