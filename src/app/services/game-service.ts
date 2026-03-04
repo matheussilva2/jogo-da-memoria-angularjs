2ce39008dbbb3b5f25881a97fde73f85b056228b
Matheus <matheusnascimentoeoq@gmail.com>
2026-03-04 11:32:42 -0300

    feat: flip card, unflip cards and shuffle cards method

--- src/app/services/game-service.ts
+++ src/app/services/game-service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { GAME_DIFFICULTIES, GAME_MODES, ICard, IGameDifficulty, IGameMode } from "../constants/game";
 import { Router } from "@angular/router";
+import { delay } from "../utils/delay";
 
 @Injectable({
   providedIn: 'root',
@@ -12,8 +13,10 @@ export class GameService {
   readonly game_difficulty = signal<IGameDifficulty | null>(null);
   readonly remaining_time = signal(0);
   readonly moves = signal(0);
+  readonly match_pairs = signal<[number, number][]>([]);
 
   private readonly router = inject(Router);
+  public cards_flipped = signal<{card_one: number, card_two: number}>({ card_one: -1, card_two: -1 });
 
   initGame(gamemode: string, difficulty: string) {
     const game_config = this.getGameConfig(gamemode, difficulty);
@@ -21,7 +24,8 @@ export class GameService {
       this.router.navigate(['/jogar']);
     } else {
       this.game_mode.set(game_config.gamemode);
-      this.game_difficulty.set(game_config.difficulty)
+      this.game_difficulty.set(game_config.difficulty);
+      this.shuffleCards();
       this.startGame();
     }
   }
@@ -30,8 +34,57 @@ export class GameService {
     this.game_state.set('playing');
   }
 
-  checkForMatch(card_index_1: number, card_index_2:number):boolean {
-    return this.cards()[card_index_1].content === this.cards()[card_index_2].content;
+  unflipCards() {
+    this.cards_flipped.set({
+      card_one: -1,
+      card_two: -1
+    });
+  }
+
+  shuffleCards() {
+    const difficulty = this.game_difficulty();
+    if(!difficulty) return false;
+    
+    const new_cards: ICard[] = [];
+    
+    if(this.game_difficulty()) {
+      for(let i = 0; i < difficulty.items_count; i++) {
+        new_cards.push({
+          content: "/assets/cards/animals/cat-face.png",
+          isFlipped: false,
+          isLocked: false
+        });
+      }
+    }
+
+    this.cards.set(new_cards);
+
+    return true;
+  }
+
+  async checkForMatch() {
+    const card_one_content = this.cards()[this.cards_flipped().card_one].content;
+    const card_two_content = this.cards()[this.cards_flipped().card_two].content
+    const match = card_one_content === card_two_content;
+
+    if(match) {
+      const updated_cards = this.cards();
+      updated_cards[this.cards_flipped().card_one].isLocked = true;
+      updated_cards[this.cards_flipped().card_two].isLocked = true;
+      this.cards.set(updated_cards);
+
+      this.match_pairs.update(prev => [...prev, [this.cards_flipped().card_one, this.cards_flipped().card_two]]);
+
+      this.cards_flipped.set({
+        card_one: -1,
+        card_two: -1
+      });
+      return true;
+    } else {
+      await delay(500);
+      this.unflipCards();
+      return false;
+    }
   }
 
   getGameMode() {
@@ -55,5 +108,31 @@ export class GameService {
       gamemode: gamemode_data[0],
       difficulty: difficulty_data[0]
     };
+  }
+
+  flipCard(card_index: number) {
+    if(this.cards_flipped().card_one !== -1 && this.cards_flipped().card_two !== -1) return;
+
+    if(this.cards_flipped().card_one !== -1) {
+      if(this.cards_flipped().card_one === card_index) {
+        return;
+      } else {
+        this.cards_flipped.update(prev => (
+          {
+            card_one: prev.card_one,
+            card_two: card_index
+          }
+        ));
+        
+        this.checkForMatch();
+      }
+    } else {
+      this.cards_flipped.set(
+        {
+          card_one: card_index,
+          card_two: -1
+        }
+      );
+    }
   }
 }
